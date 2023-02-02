9404b662d4348fb6f745655bd72c82119066de14
alwalk0 <walkernotwalker@gmail.com>
2023-02-02 19:04:08 +0100

    temporary refactor to one file:

--- framework/create_view_function.py
+++ framework/create_view_function.py
@@ -2,6 +2,7 @@
 from starlette.responses import JSONResponse
 
 
+
 def set_response(method, type, results, fields):
     match type:
         case 'json':
@@ -27,7 +28,8 @@ async def get_query(request, method, table):
             return table.insert().values(data)
 
 
-def get_execute_function(method, database, query):
+def get_execute_function(method, query):
+    database = get_database()
     match method:
         case 'GET':
             return database.fetch_all(query)
@@ -36,12 +38,12 @@ def get_execute_function(method, database, query):
 
 
 
-def create_view_function(table, database, method, return_type, fields):
+def create_view_function(table, method, return_type, fields):
 
     async def view_function(request):
 
         query = await get_query(request, method, table)
-        results = await get_execute_function(method, database, query)
+        results = await get_execute_function(method, query)
 
         return set_response(method, return_type, results, fields=fields)
 
--- framework/parse_yaml.py
+++ framework/parse_yaml.py
@@ -9,12 +9,76 @@ from .create_view_function import create_view_function
 import importlib
 import importlib.machinery
 from pathlib import Path
+from starlette.responses import JSONResponse
 
 
-module_path = Path(__file__).parent.parent
-modulename = importlib.machinery.SourceFileLoader('articles', '/Users/alan/Desktop/Projects/Code/telstarbasic/articles.py').load_module()
-module = getattr(modulename, 'database')
-print(module)
+def set_response(method, type, results, fields):
+    match type:
+        case 'json':
+            match method:
+                case 'GET':
+                    content = [
+                    {
+                        field: result[field] for field in fields
+                    }
+                    for result in results
+                    ]
+                    return JSONResponse(content)
+                case 'POST':
+                    return JSONResponse({'OK':'OK'})
+            
+
+async def get_query(request, method, table):
+    match method:
+        case 'GET':
+            return table.select()
+        case 'POST':
+            data = await request.json()
+            return table.insert().values(data)
+
+
+def get_execute_function(method, query, database):
+
+    match method:
+        case 'GET':
+            return database.fetch_all(query)
+        case 'POST':
+            return database.execute(query)
+
+
+
+def create_view_function(table, method, database, return_type, fields):
+
+    async def view_function(request):
+
+        query = await get_query(request, method, table)
+        results = await get_execute_function(method, query, database)
+
+        return set_response(method, return_type, results, fields=fields)
+
+    return view_function 
+
+
+
+def read_key_from_config(config:str, key:str)-> str:
+    with open(config, 'r') as file:
+        yaml_dict = yaml.safe_load(file)
+    
+    return yaml_dict.get(key)
+
+
+
+
+def get_database():
+
+    models = read_key_from_config('main.yml', 'models')
+    database = read_key_from_config('main.yml', 'database')
+    module_path = Path(__file__).parent.parent
+    file_path = str(module_path) + '/' + models
+    modulename = importlib.machinery.SourceFileLoader(models.removesuffix('.py'), file_path).load_module()
+
+    return getattr(modulename, database)
+    
 
 
 
@@ -34,7 +98,7 @@ def create_routes_list(database, yaml_dict: dict) -> list:
                 return_type = v['return']
                 fields = (v['fields']).split(', ')
 
-                app_routes.append(Route(url, endpoint=create_view_function(table=eval(db_table), database=database, method=method, return_type=return_type, fields=fields),methods=[method]))
+                app_routes.append(Route(url, endpoint=create_view_function(table=eval(db_table), method=method, database=database, return_type=return_type, fields=fields),methods=[method]))
 
             elif endpoint == 'view':
                 template = v['template']
@@ -49,7 +113,7 @@ def create_app_from_config(config:str)-> Starlette:
     with open(config, 'r') as file:
         yaml_dict = yaml.safe_load(file)
 
-    database = getattr(modulename, 'database')    
+    database = get_database()
 
     app_routes = create_routes_list(database, yaml_dict=yaml_dict)    
 
@@ -60,15 +124,5 @@ def create_app_from_config(config:str)-> Starlette:
     )
 
     return app
-
-
-
-
-def read_key_from_config(config:str, key:str)-> str:
-    with open(config, 'r') as file:
-        yaml_dict = yaml.safe_load(file)
-    
-    return yaml_dict.get(key)
-
 
 
