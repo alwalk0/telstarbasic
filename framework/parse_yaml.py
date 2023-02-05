3c7d6cb44cb06433fa00e518132d37a5f799d3a7
alwalk0 <walkernotwalker@gmail.com>
2023-02-05 01:36:57 +0100

    more refactoring, cleanup

--- framework/parse_yaml.py
+++ framework/parse_yaml.py
@@ -9,9 +9,11 @@ import importlib.machinery
 from pathlib import Path
 from starlette.responses import JSONResponse
 from starlette.templating import Jinja2Templates
+from databases import Database
+from starlette.requests import Request
 
 
-def set_response(method, type, results, fields):
+def set_response(method: str, type, results, fields):
     match type:
         case 'json':
             match method:
@@ -27,7 +29,7 @@ def set_response(method, type, results, fields):
                     return JSONResponse({'OK':'OK'})
             
 
-async def get_query(request, method, table):
+async def get_query(request: Request, method: str, table):
     match method:
         case 'GET':
             return table.select()
@@ -36,7 +38,7 @@ async def get_query(request, method, table):
             return table.insert().values(data)
 
 
-def get_execute_function(method, query, database):
+def get_execute_function(method: str, query, database: Database):
 
     match method:
         case 'GET':
@@ -45,20 +47,20 @@ def get_execute_function(method, query, database):
             return database.execute(query)
 
 
-def create_view_function(endpoint, database):
+def create_view_function(specs:dict, database: Database) -> callable:
 
-    method = endpoint['method']
-    return_type = endpoint['return']
-    fields = (endpoint['fields']).split(', ')
-    table = endpoint['db_table']
+    method = specs['method']
+    return_type = specs['return']
+    fields = (specs['fields']).split(', ')
+    table = specs['db_table']
 
     async def view_function(request):
 
-        query = await get_query(request, method, table=eval(table))
+        query = await get_query(request, method=method, table=eval(table))
 
-        results = await get_execute_function(method, query, database)
+        results = await get_execute_function(method=method, query=query, database=database)
 
-        return set_response(method, return_type, results, fields=fields)
+        return set_response(method=method, type=return_type, results=results, fields=fields)
 
     return view_function 
 
@@ -70,7 +72,7 @@ def read_key_from_config(config:str, key:str)-> str:
     return yaml_dict.get(key)
 
 
-def get_database(models, database):
+def get_database(models:str, database:str) -> Database:
 
     module_path = Path(__file__).parent.parent
     file_path = str(module_path) + '/' + models
@@ -79,41 +81,40 @@ def get_database(models, database):
     return getattr(modulename, database)
     
 
-def create_routes_list(yaml_dict: dict, database, templates, models) -> list:
+def create_routes_list(yaml_dict: dict, database: Database, templates: Jinja2Templates) -> list:
     app_routes = []
-    for key,value in yaml_dict.items():
-        for k, v in value.items():
-            endpoint = v['endpoint']
-            url = v['url']
-            if endpoint == 'api':
-                    
-                method = v['method']
 
-                app_routes.append(Route(url, endpoint=create_view_function(v, database),methods=[method]))
+    for view, specs in yaml_dict.items():
+        endpoint = specs['endpoint']
+        url = specs['url']
+        if endpoint == 'api':
+                
+            method = specs['method']
+            app_routes.append(Route(url, endpoint=create_view_function(specs=specs, database=database),methods=[method]))
 
-            elif endpoint == 'view':
-                template = v['template']
-                app_routes.append(Route(url, lambda request:templates.TemplateResponse(template, {'request':request})))
-                app_routes.append(Mount('/static', app=StaticFiles(directory='statics'), name='static'))     
+        elif endpoint == 'view':
+            template = specs['template']
+            app_routes.append(Route(url, lambda request:templates.TemplateResponse(template, {'request':request})))
+            app_routes.append(Mount('/static', app=StaticFiles(directory='statics'), name='static'))     
 
     return app_routes      
 
 
-def create_app_from_config(config:str)-> Starlette:
-
-    with open(config, 'r') as file:
-        main_config = yaml.safe_load(file)
+def create_app_from_config(main_config:str)-> Starlette:
 
     views_config = main_config['views']
+    models = main_config['models']
+    database = main_config['database']
+    templates = main_config['templates']
 
     with open(views_config, 'r') as file:
         views_dict = yaml.safe_load(file)    
 
-    database = get_database(main_config['models'], main_config['database'])
+    database = get_database(models, database)
 
-    templates = Jinja2Templates(directory=str(main_config['templates']))
+    templates = Jinja2Templates(directory=str(templates))
 
-    app_routes = create_routes_list(yaml_dict=views_dict, database=database, templates=templates, models=main_config['models'])    
+    app_routes = create_routes_list(yaml_dict=views_dict, database=database, templates=templates)    
 
     app = Starlette(
         routes=app_routes,
--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 import uvicorn
+import yaml
 from framework.parse_yaml import create_app_from_config, read_key_from_config
 
 
 MAIN_CONFIG_PATH = 'main.yml'
 
-# views_config_name = read_key_from_config(MAIN_CONFIG_PATH, 'views')
+with open(MAIN_CONFIG_PATH, 'r') as file:
+    main_config = yaml.safe_load(file)
 
-app = create_app_from_config(MAIN_CONFIG_PATH)
-host = read_key_from_config(MAIN_CONFIG_PATH, 'host')
-port = read_key_from_config(MAIN_CONFIG_PATH, 'port')
+app = create_app_from_config(main_config)
+host = main_config['host']
+port = main_config['port']
 
 
 if __name__ == "__main__":
